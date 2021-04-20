e4237367ce9f5fdca25342f2f67c7626c0b59ea6
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-04-20 21:11:28 +0800

    restructure deck components

--- components/Cards/Deck.tsx
+++ components/Cards/Deck.tsx
@@ -28,92 +28,92 @@ const Deck: React.FC<DeckProps> = ({ pokemons, cardIndex, exitX, setExitX, index
 
   return (
     <div className="card-wrapper">
-     <AnimatePresence>
-          {pokemons.length >= 3 && cardIndex + 2 < pokemons.length + 1 && (
-            <FramerCard
-              key={cardIndex + 2}
-              index={cardIndex + 2}
-              initial={{
-                scale: 0,
-                y: 0,
-                opacity: 0,
-              }}
-              animate={{
-                scale: 0.8,
-                y: -75,
-                opacity: 1,
-                boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
-              }}
-              transition={{
-                scale: { duration: 0.5 },
-              }}
-            >
-              {pokemons && cardIndex + 2 < pokemons.length ? (
-                <p>{pokemons[cardIndex + 2].name}</p>
-              ) : (
-                <p>end</p>
-              )}
-            </FramerCard>
-          )}
-          {pokemons.length >= 2 && cardIndex + 1 < pokemons.length + 1 && (
-            <FramerCard
-              key={cardIndex + 1}
-              index={cardIndex + 1}
-              animate={{
-                scale: 0.9,
-                y: -40,
-                opacity: 1,
-                boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
-              }}
-              transition={{
-                scale: { duration: 0.5 },
-              }}
-            >
-              {pokemons && cardIndex + 1 < pokemons.length ? (
-                <p>{pokemons[cardIndex + 1].name}</p>
-              ) : (
-                <p>end</p>
-              )}
-            </FramerCard>
-          )}
-          {pokemons.length >= 1 && cardIndex < pokemons.length + 1 && (
-            <FramerCard
-              key={cardIndex}
-              index={cardIndex}
-              initial={{
-                scale: 1,
-                y: 40,
-                opacity: 1,
-              }}
-              animate={{
-                scale: 1,
-                y: 0,
-                opacity: 1,
-                boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
-              }}
-              transition={{
-                scale: { duration: 0.15 },
-              }}
-              whileTap={{
-                cursor: "grabbing",
-              }}
-              whileHover={{
-                cursor: "grab",
-              }}
-              handleDragEnd={handleDragEnd}
-              exitX={exitX}
-              setExitX={setExitX}
-              setIndex={setIndex}
-              drag="x"
-            >
-              {pokemons && cardIndex < pokemons.length ? (
-                <p>{pokemons[cardIndex].name}</p>
-              ) : (
-                <p>end</p>
-              )}
-            </FramerCard>
-          )}
-        </AnimatePresence>
+      <AnimatePresence>
+        {pokemons.length >= 3 && cardIndex + 2 < pokemons.length + 1 && (
+          <FramerCard
+            key={cardIndex + 2}
+            index={cardIndex + 2}
+            initial={{
+              scale: 0,
+              y: 0,
+              opacity: 0,
+            }}
+            animate={{
+              scale: 0.8,
+              y: -75,
+              opacity: 1,
+              boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
+            }}
+            transition={{
+              scale: { duration: 0.5 },
+            }}
+          >
+            {pokemons && cardIndex + 2 < pokemons.length ? (
+              <p>{pokemons[cardIndex + 2].name}</p>
+            ) : (
+              <p>end</p>
+            )}
+          </FramerCard>
+        )}
+        {pokemons.length >= 2 && cardIndex + 1 < pokemons.length + 1 && (
+          <FramerCard
+            key={cardIndex + 1}
+            index={cardIndex + 1}
+            animate={{
+              scale: 0.9,
+              y: -40,
+              opacity: 1,
+              boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
+            }}
+            transition={{
+              scale: { duration: 0.5 },
+            }}
+          >
+            {pokemons && cardIndex + 1 < pokemons.length ? (
+              <p>{pokemons[cardIndex + 1].name}</p>
+            ) : (
+              <p>end</p>
+            )}
+          </FramerCard>
+        )}
+        {pokemons.length >= 1 && cardIndex < pokemons.length + 1 && (
+          <FramerCard
+            key={cardIndex}
+            index={cardIndex}
+            initial={{
+              scale: 1,
+              y: 40,
+              opacity: 1,
+            }}
+            animate={{
+              scale: 1,
+              y: 0,
+              opacity: 1,
+              boxShadow: "0 5px 25px 1px rgba(0,0,0,.5)",
+            }}
+            transition={{
+              scale: { duration: 0.15 },
+            }}
+            whileTap={{
+              cursor: "grabbing",
+            }}
+            whileHover={{
+              cursor: "grab",
+            }}
+            handleDragEnd={handleDragEnd}
+            exitX={exitX}
+            setExitX={setExitX}
+            setIndex={setIndex}
+            drag="x"
+          >
+            {pokemons && cardIndex < pokemons.length ? (
+              <p>{pokemons[cardIndex].name}</p>
+            ) : (
+              <p>end</p>
+            )}
+          </FramerCard>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
